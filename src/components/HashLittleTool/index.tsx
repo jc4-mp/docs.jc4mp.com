b15e56cca19b21a4e81dd039fe0d895da707f5fd
benank <gh@benank.com>
2025-01-18 07:16:30 -1000

    Update output gap

--- src/components/HashLittleTool/index.tsx
+++ src/components/HashLittleTool/index.tsx
@@ -30,57 +30,66 @@ export default function HashLittle(): JSX.Element {
           className={styles.input}
         />
       </div>
-      <div className={styles.outputContainer}>
-        <span className={styles.label}>uint32</span>
-        <input type="text" readOnly value={output} className={styles.output} />
-        <button
-          onClick={() => {
-            copyToClipboard(output);
-            setCopiedUnsigned(true);
-            setTimeout(() => setCopiedUnsigned(false), 2000);
-          }}
-          className={clsx(styles.copyButton, copiedUnsigned && styles.copied)}
-        >
-          {copiedUnsigned ? "Copied!" : "Copy"}
-        </button>
-      </div>
-      <div className={styles.outputContainer}>
-        <span className={styles.label}>int32</span>
-        <input
-          type="text"
-          readOnly
-          value={parseInt(output) >> 0}
-          className={styles.output}
-        />
-        <button
-          onClick={() => {
-            copyToClipboard((parseInt(output) >> 0).toString());
-            setCopiedSigned(true);
-            setTimeout(() => setCopiedSigned(false), 2000);
-          }}
-          className={clsx(styles.copyButton, copiedSigned && styles.copied)}
-        >
-          {copiedSigned ? "Copied!" : "Copy"}
-        </button>
-      </div>
-      <div className={styles.outputContainer}>
-        <span className={styles.label}>hex</span>
-        <input
-          type="text"
-          readOnly
-          value={`0x${parseInt(output).toString(16).toUpperCase()}`}
-          className={styles.output}
-        />
-        <button
-          onClick={() => {
-            copyToClipboard(`0x${parseInt(output).toString(16).toUpperCase()}`);
-            setCopiedHex(true);
-            setTimeout(() => setCopiedHex(false), 2000);
-          }}
-          className={clsx(styles.copyButton, copiedHex && styles.copied)}
-        >
-          {copiedHex ? "Copied!" : "Copy"}
-        </button>
+      <div className={styles.outputs}>
+        <div className={styles.outputContainer}>
+          <span className={styles.label}>uint32</span>
+          <input
+            type="text"
+            readOnly
+            value={output}
+            className={styles.output}
+          />
+          <button
+            onClick={() => {
+              copyToClipboard(output);
+              setCopiedUnsigned(true);
+              setTimeout(() => setCopiedUnsigned(false), 2000);
+            }}
+            className={clsx(styles.copyButton, copiedUnsigned && styles.copied)}
+          >
+            {copiedUnsigned ? "Copied!" : "Copy"}
+          </button>
+        </div>
+        <div className={styles.outputContainer}>
+          <span className={styles.label}>int32</span>
+          <input
+            type="text"
+            readOnly
+            value={parseInt(output) >> 0}
+            className={styles.output}
+          />
+          <button
+            onClick={() => {
+              copyToClipboard((parseInt(output) >> 0).toString());
+              setCopiedSigned(true);
+              setTimeout(() => setCopiedSigned(false), 2000);
+            }}
+            className={clsx(styles.copyButton, copiedSigned && styles.copied)}
+          >
+            {copiedSigned ? "Copied!" : "Copy"}
+          </button>
+        </div>
+        <div className={styles.outputContainer}>
+          <span className={styles.label}>hex</span>
+          <input
+            type="text"
+            readOnly
+            value={`0x${parseInt(output).toString(16).toUpperCase()}`}
+            className={styles.output}
+          />
+          <button
+            onClick={() => {
+              copyToClipboard(
+                `0x${parseInt(output).toString(16).toUpperCase()}`
+              );
+              setCopiedHex(true);
+              setTimeout(() => setCopiedHex(false), 2000);
+            }}
+            className={clsx(styles.copyButton, copiedHex && styles.copied)}
+          >
+            {copiedHex ? "Copied!" : "Copy"}
+          </button>
+        </div>
       </div>
     </div>
   );
